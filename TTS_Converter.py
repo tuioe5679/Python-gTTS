0908ca9fa7c7a9709b9412dad02605ece53b59d6
tuioe <qodlgks123@naver.com>
2022-09-15 22:46:09 +0900

    Update TTS_Converter.py

--- TTS_Converter.py
+++ TTS_Converter.py
@@ -4,19 +4,16 @@ import pymysql
 conn = pymysql.connect(host='localhost',user='root',passwd='1234',db="kiosk",charset="utf8")
 cur = conn.cursor()
 
-sql = "select name from product"
-cur.execute(sql)
+sql = "select name from product" //  SQL문   
+cur.execute(sql) // 쿼리 실행 
 
-try:
-    for row in cur:
-            f = row[0]
-            t = row[0]
-            l = "ko"
+for row in cur: 
+    title = row[0] //제목
+    content  = row[0] //내용
+    language = "ko" //언어 (ko = 한국언어 발음) 
 
-            tts = gTTS(t,lang=l)
-            tts.save(f+".mp3")
-except KeyboardInterrupt:
-    pass
-		
+    tts = gTTS(t,lang=l)
+    tts.save(f+".mp3")
+
 
 
